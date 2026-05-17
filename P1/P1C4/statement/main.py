6aff817c2aa2ba4d3b76ea0fe8446e39254aa91b
Chaitanya02119 <bylapudichaitanya09@gmail.com>
2026-05-17 11:23:42 +0530

    Update main.py

--- P1/P1C4/statement/main.py
+++ P1/P1C4/statement/main.py
@@ -1,1 +1,5 @@
-# Write your code here!
+# Write your code here!name = "chaitanya";
+age = 17;
+print(f"my name is { name} and i am {age} years old");
+age = age+10;
+print(f"My name is {name} and i am now {age} years old in 10 years");
